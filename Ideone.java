16631e5047dd76c3e749381fe02bcc25fc5bf163
root <root@ip-172-31-16-231.us-west-2.compute.internal>
2018-09-28 07:24:52 +0000

    Java Test

---
+++ Ideone.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+public class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("Hello World");
+		}
+	}
+}
