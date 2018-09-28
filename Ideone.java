7a8c1f945f5760ab99874497f5f47599a87e0a52
root <root@ip-172-31-16-231.us-west-2.compute.internal>
2018-09-28 08:58:03 +0000

    Some Changes

--- Ideone.java
+++ Ideone.java
@@ -8,6 +8,7 @@ public class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		System.out.println("Welcome to JAva");
 		for(int i=0; i<10; i++)
 		{
 			System.out.println("Hello World");
